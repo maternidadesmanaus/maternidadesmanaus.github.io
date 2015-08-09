41c797d060aa71d794ed3d51413e01070f9c8587
Andrews Lince <andrews.lince@gmail.com>
2015-08-09 10:37:47 -0400

    Ajustando módulo 'watch', do Grunt, para buildar o projeto quando as imagens forem alteradas

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
                     'img/*'
                 ],
 
-                tasks : [ 'imagemin' ]
+                tasks : [ 'imagemin', 'exec:build', 'concat' ]
             },
 
             scripts : {
