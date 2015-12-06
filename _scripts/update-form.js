e0b20cb6a2a2b263d807594daae07afd8ff2b81e
Andrews Lince <andrews.lince@gmail.com>
2015-12-06 04:05:54 -0400

    Atualizando parser do HTML do formulário do Google

--- _scripts/update-form.js
+++ _scripts/update-form.js
@@ -1,18 +1,26 @@
 var casper = require('casper').create();
 var fs = require("fs");
-var googleFormUrl = 'https://docs.google.com/forms/d/1UVB3EqhdY4abRrzRegIn19ef1bhPaX4STuFvrbpwSIg/viewform';
+// var googleFormUrl = 'https://docs.google.com/forms/d/1UVB3EqhdY4abRrzRegIn19ef1bhPaX4STuFvrbpwSIg/viewform';  // PRD
+var googleFormUrl = 'https://docs.google.com/forms/d/1egfBCuUuQiYxtpo6_fUrMNUb_RnajuCkMkVekWlfsDs/viewform';  // BETA
 
 console.log("\nacessando formulário do google...");
 casper.start(googleFormUrl, function() {
 
     console.log("parseando html do formulário...");
-    var form = this.evaluate(function() {
-        return '<form action="https://docs.google.com/forms/d/1UVB3EqhdY4abRrzRegIn19ef1bhPaX4STuFvrbpwSIg/formResponse" method="POST" target="_self">'
-            + document
-                .querySelector(".freebirdFormviewerViewCenteredContent form")
-                .innerHTML
-                .replace(/required\=\"\"/g, '');
-            + '</form>';
+    var htmlForm = this.evaluate(function() {
+        var content = document.querySelector(".freebirdFormviewerViewCenteredContent");
+
+        content.removeChild(
+            document.querySelector(".freebirdFormviewerViewCenteredContent > div")
+        );
+
+        return content
+            .innerHTML
+            .replace(/required\=\"\"/g, '')
+            .replace(
+                "<span>Enviar</span>",
+                '<input value="Enviar" id="submit-form" type="submit">'
+            );
     });
 
     console.log("gravando arquivo com o formulário...");
@@ -20,7 +28,7 @@ casper.start(googleFormUrl, function() {
         fs.absolute(".")
             + fs.separator + "_includes"
             + fs.separator + "google-form.html",
-        form,
+        htmlForm,
         "w"
     );
 });
